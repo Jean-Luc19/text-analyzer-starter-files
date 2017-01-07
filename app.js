b1b0071867bfbb1283e1cc4a8896b190079efc10
Jean-Luc19 <aticus19@gmail.com>
2017-01-07 09:54:54 -0700

    finished average sentence length function

--- app.js
+++ app.js
@@ -3,18 +3,32 @@ function countWords(text) {
 	return wordArray.length;
 }
 function getUniqueWordCount(text) {
-	text = text.replace
+	text = text.replace(/\./g, '').split(' ');
+	var uniqueWordArray = [];
+	for (var i = 0; i < text.length; i++) {
+		if (uniqueWordArray.indexOf(text[i]) === - 1) {
+			uniqueWordArray.push(text[i]);
+		}
+	}
+	return uniqueWordArray;
 }
 function getSentenceLengths (text) {
 	var sentences = text.split('.');
-	return sentences.
+	var individualSentenceWordArray = [];
+	var totalSentenceLength = 0;
+	var numSentences = sentences.length;
+	for (var i = 0; i < sentences.length; i++) {
+		individualSentenceWordArray = sentences[i].split(' ');
+		totalSentenceLength += individualSentenceWordArray.length;
+	}
+	return totalSentenceLength;
 
 }
 
 
 var text = 'string is a string. yes it is';
 
+ 
+
+
 debug(getSentenceLengths(text));
-
-
-// this is a comment I'm adding
