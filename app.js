e9f97b43354996400a8f6d20cfb7c2393e0be3e4
Jean-Luc19 <aticus19@gmail.com>
2017-01-07 12:36:23 -0700

    first working draft of apps.js

--- app.js
+++ app.js
@@ -1,34 +1,64 @@
+var $inputText = $('textarea');
+
 function countWords(text) {
-	var wordArray = text.split(' ');
+	var wordArray = text.replace(/[\.!?]/g, '').split(' ');
+   
 	return wordArray.length;
 }
 function getUniqueWordCount(text) {
-	text = text.replace(/\./g, '').split(' ');
+	text = text.replace(/[\.!?]/g, '').split(' ');
 	var uniqueWordArray = [];
 	for (var i = 0; i < text.length; i++) {
 		if (uniqueWordArray.indexOf(text[i]) === - 1) {
 			uniqueWordArray.push(text[i]);
 		}
 	}
-	return uniqueWordArray;
+	return uniqueWordArray.length;
+}
+function getAverageWordLength(text) {
+  var cleanText = text.replace(/[\.!?]/g, '').split(' ');
+  var wordLengths = 0;
+  for (var i = 0; i < cleanText.length; i++) {
+    wordLengths += cleanText[i].length;
+  }
+  return wordLengths / cleanText.length;
+  
 }
 function getSentenceLengths (text) {
-	var sentences = text.split('.');
-	var individualSentenceWordArray = [];
-	var totalSentenceLength = 0;
-	var numSentences = sentences.length;
-	for (var i = 0; i < sentences.length; i++) {
-		individualSentenceWordArray = sentences[i].split(' ');
-		totalSentenceLength += individualSentenceWordArray.length;
-	}
-	return totalSentenceLength;
-
+	var sentences = text.replace(/[!?]/g, '.').split('.').length - 1;
+	return countWords(text) / sentences;
 }
+function displayWordCount(text) {
+  $('.js-word-count').text(countWords(text));
+}
+function displayUniqueWordCount(text) {
+  $('.js-unique-words').append(getUniqueWordCount(text));
+}
+function displayAverageWordLength(text) {
+  $('.js-average-word-length').append(getAverageWordLength(text));
+}
+function displayAverageSentenceLength(text) {
+  $('.js-average-sentence-length').append(getSentenceLengths(text));
+}
+function handleFormSubmit(){
+  $('button').click(function(event){
+    event.preventDefault();
+    $('dl').removeClass('hidden');
+    var theText = $inputText.val();
+    displayWordCount(theText)
+    displayUniqueWordCount(theText);
+    displayAverageWordLength(theText)
+    displayAverageSentenceLength(theText)
+    
+  });
+}
+$(function(){
+  handleFormSubmit();
+});
 
 
-var text = 'string is a string. yes it is';
 
  
 
 
-debug(getSentenceLengths(text));
+
